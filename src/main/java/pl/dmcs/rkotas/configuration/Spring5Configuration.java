e482e08591823c374042628ae578cf34267a1747
raqiet <dawidm@int.pl>
2019-10-12 16:23:37 +0200

    Spring init

---
+++ src/main/java/pl/dmcs/rkotas/configuration/Spring5Configuration.java
@@ -0,0 +1,24 @@
+package pl.dmcs.rkotas.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.dmcs.rkotas")
+public class Spring5Configuration implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
+
